02bdcf9b50f76f846654192b0da3a06ebe446140
GIMI <podivinsky.adam@seznam.cz>
2020-12-14 01:23:27 +0100

    Version update

--- server/main.lua
+++ server/main.lua
@@ -1,5 +1,5 @@
 --================================--
---       FIRE SCRIPT v1.6.3       --
+--       FIRE SCRIPT v1.6.4       --
 --  by GIMI (+ foregz, Albo1125)  --
 --      License: GNU GPL 3.0      --
 --================================--
@@ -8,7 +8,7 @@
 --         VERSION CHECK          --
 --================================--
 
-Version = "1.6.3"
+Version = "1.6.4"
 LatestVersionFeed = "https://api.github.com/repos/gimicze/firescript/releases/latest"
 
 Citizen.CreateThread(
