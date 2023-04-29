242593f2bea0068dd955202cfb0922f7a1d36694
gimicze <38395100+gimicze@users.noreply.github.com>
2023-04-29 20:28:56 +0200

    Update main.lua
    Changed ESX object obtaining mechanism.

--- server/main.lua
+++ server/main.lua
@@ -554,12 +554,10 @@ AddEventHandler(
 --================================--
 
 if Config.Dispatch.enabled and Config.Dispatch.enableESX then
-    ESX = nil
-
-    TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+    ESX = exports["es_extended"]:getSharedObject()
 
     local allowedJobs = {}
-	local firefighterJobs = Config.Fire.spawner.firefighterJobs or {}
+				local firefighterJobs = Config.Fire.spawner.firefighterJobs or {}
 
     if type(Config.Dispatch.enableESX) == "table" then
         for k, v in pairs(Config.Dispatch.enableESX) do
